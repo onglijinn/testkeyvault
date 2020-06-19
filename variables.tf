d5c594317736d0102b574e79aaa1f080ffc97316
onglijinn <65584029+onglijinn@users.noreply.github.com>
2020-06-19 14:06:32 +1000

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "location"{
+    default = "Australia Southeast"
+}
+
+variable "name" {
+    default = "terraformdemorun10" // name of the resource group has to be a global unique name 
+}
